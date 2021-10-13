c1b7af21e9c3a650c0cba2f8564bb96b066f7d89
LuisFerrufino <a2020115239@estudiantes.upsa.edu.bo>
2021-10-13 17:55:29 -0400

    Se creó el proyecto del 2do parcial y el contenido de la API de Naipes

---
+++ LuisFerrrufino2DOPARCIAL/Models/Naipe.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LuisFerrrufino2DOPARCIAL.Models
+{
+    public class Naipe
+    {
+       [Key]
+        public string NaipeId { get; set; }
+        [Required]
+        [Display(Name = "Nombre y tipo de la Carta ")]
+        public string Nombre { get; set; }
+        [Url]
+        [Required]
+        [Display(Name = "Link de la carta")]
+        public string  Imagen { get; set; }
+    }
+}
