7ca858e03b080995054c1daefa02a65e2b26528f
LuisFerrufino <a2020115239@estudiantes.upsa.edu.bo>
2021-10-13 18:14:57 -0400

    Se creó Data, la conexón con la base de datos y el AppDbContext

---
+++ LuisFerrrufino2DOPARCIAL/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using LuisFerrrufino2DOPARCIAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LuisFerrrufino2DOPARCIAL.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public DbSet<Naipe> Naipes { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+
+        {
+
+
+
+        }
+    }
+}
